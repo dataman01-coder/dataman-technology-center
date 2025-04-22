6e6394dd9066a379e2996352a8181aa245068bdd
Prosper Darko <darkoprosper42@gmail.com>
2025-04-22 01:46:04 +0000

    Add files via upload
    Files were uploaded from the main folder into the git hub repo

---
+++ script.js
@@ -0,0 +1,10 @@
+// Javascript toggle menu
+var navLinks=document.getElementById("navLinks")
+
+function showMenu(){
+    navLinks.style.display="unset";
+}
+
+function hideMenu(){
+    navLinks.style.display="none";
+}
